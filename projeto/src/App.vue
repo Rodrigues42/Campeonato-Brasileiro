24caf8261bdb53921ea509556f8e66374e353d6c
Leonardo <leozikasantos73@gmail.com>
2021-09-30 10:33:21 -0300

    Adiconando tema dark

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Inicio</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <style>
