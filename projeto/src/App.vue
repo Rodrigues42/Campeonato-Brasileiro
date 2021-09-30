f00f253d979d666e2319d0714e2e43844f0eb96e
Leonardo <leozikasantos73@gmail.com>
2021-09-30 10:44:47 -0300

    Adicionando o componente TopBar

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <v-app>
+    <TopBar/>
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
+<script>
+import TopBar from './components/shared/TopBar.vue'
 
+export default {
+  name:'App',
+  components: {
+    TopBar
+  }
+}
+</script>
 <style>
 
 </style>
